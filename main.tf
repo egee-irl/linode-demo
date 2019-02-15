a8953fe03e54d13931590f16271e4df49ea412ad
egee-irl <brian@egee.io>
2019-02-14 21:56:28 -0800

    added output, stackscript id

--- main.tf
+++ main.tf
@@ -2,8 +2,13 @@ module "linode" {
   source           = "github.com/egee-irl/terraform-linode-instance.git"
   authorized_keys  = ["${chomp(file(var.pub_key))}"]
   authorized_users = ["egee"]
-  type             = "g6-standard-6"
+  type             = "g6-nanode-1"                                       # https://developers.linode.com/api/v4#operation/getLinodeTypes
   region           = "us-central"
-  label            = "primary"
+  label            = "demo"
   image            = "linode/ubuntu18.04"
+  stackscript_id   = "394412"
+}
+
+output "instance_ip" {
+  value = "${module.linode.public_ip}"
 }
